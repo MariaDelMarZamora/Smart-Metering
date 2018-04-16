e97cf1054dacfcbece6ae7f99948324869a3607e
MariaDelMarZamora <mariaz09@gmail.com>
2018-04-16 12:22:25 +0200

    Create strings from  manual search

--- Prep/Query Dev.R
+++ Prep/Query Dev.R
@@ -26,11 +26,20 @@ pws <- pws %>% separate(col = `Author & Year`, into = c("Author", "Year"), sep =
 
 # generate individual strings
   # WoS - pattern (TI=("title") AND AU=("Abdalla") AND PY=(2015))
-  # Scopus - pattern (TITLE("") AND AUTH("Abdalla") AND PUBYEAR IS 2015 )
+  # Scopus - pattern (TITLE("") AND AUTH("Abdalla") AND PUBYEAR IS 2015 ))
 
 pws <- pws %>% 
-  mutate(WoS = paste('(TI = ("', Title, '") AND AU=("', AUshort, '" AND PY=("', Year, ')', sep = ""),
-         Scopus = paste('(TITLE("', Title, '" AND AUTH=("', AUshort,'" AND PUBYEAR IS "', Year, ')', sep = ""))
+  mutate(WoS = paste('(TI = ("', Title, '") AND AU=("', AUshort, '") AND PY=(', Year, '))', sep = ""),
+         Scopus = paste('(TITLE("', Title, '") AND AUTH("', AUshort,'") AND PUBYEAR IS ', Year, ')', sep = ""))
+
+searchStrings <- data.frame(database = c("WoS", "Scopus"),
+                            query = as.character(NA)) %>% 
+  mutate(query = as.character(query))
 
 
 #combine strings
+searchStrings$query[searchStrings$database== "WoS"] <- paste0(pws$WoS, collapse = ' OR ')
+searchStrings$query[searchStrings$database== "Scopus"] <- paste0(pws$Scopus, collapse = ' OR ')
+
+write.csv(searchStrings, file = "../Queries/strings.csv")
+
