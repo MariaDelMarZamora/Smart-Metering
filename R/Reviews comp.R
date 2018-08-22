280c662805c64bd8a13df0d0498a712c5817367f
Maria del Mar <delm@mcc-berlin.net>
2018-08-22 16:23:27 +0200

    Create this_study var Obtain possibly relevant studies (inc maybes) reconcile empty fields

--- R/Reviews comp.R
+++ R/Reviews comp.R
@@ -1,8 +1,8 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
-library(gdata)
-
+library(readxl)
+# Tomorrow: when to create the this_study variable perhaps from beginning with doc
 
 #read in Review contents table
 df <- read_excel("../Reviews Annotated.xlsx", sheet =3) 
@@ -28,18 +28,66 @@ docs <- docs %>%
                    ifelse(java == "Maybe" & netRel == 0, "maybe",
                           ifelse(kocn == "Maybe" & netRel == 0, "maybe",
                                  ifelse(delm == "Maybe" & netRel == 0, "maybe",NA)))),
-    relevant = ifelse(netRel > 0, TRUE, FALSE)
+    relevant = ifelse(netRel > 0, TRUE, FALSE),
+    query = 2801,
+    this_study = ifelse(relevant == T, "cap_rel",
+                        ifelse(relevant == F & maybe == "maybe", "cap_mayb",
+                               ifelse((relevant == F & is.na(maybe)), "cap_NOT_rel",
+                                      #                       "test")
+                                      "wrong")))
   ) %>% 
-  select(TI, PY, relevant, DOI, maybe)
+  select(TI, PY, AU, relevant, DOI, maybe,query, this_study)
+
+table(docs$this_study, useNA = "always") #change below to include this study
+
 
 #match review DOIs to  query results 
     #resulting df has only the 147 docs sourced from reviews
 df_matched <- left_join(df, docs, by = "DOI") %>% 
-  select(AU, DOI, TI.x, TI.y, PY.x, Karlin:Abrahamse_, -ACEEE, reviews, relevant, maybe) #adjust col name as reviews are added
+  select(AU.x, DOI, TI.x, TI.y, PY.x, Karlin:Abrahamse_, -ACEEE, reviews, relevant, maybe, query) #adjust col name as reviews are added
 
 table(df_matched$relevant)
 
 
 #getting those captured by query but not reviews
-filter(maybe == "maybe" | relevant == TRUE)#filter any yes or maybes
+poss_Rel <- docs %>% 
+  filter(maybe == "maybe" | relevant == TRUE) %>%  #filter any yes or maybes
+  group_by(TI) %>% 
+  arrange(TI, maybe) %>% 
+  mutate(
+    indx = row_number()
+  ) %>% 
+  filter((relevant == T & indx == 1) | 
+           (relevant == T & indx == 2) |
+           (maybe == "maybe" & max(indx) == 1 )) %>%
+  ungroup() %>% 
 
+
+
+df_matched <- full_join(df_matched, poss_Rel, by = c("DOI" = "DOI", "relevant" = "relevant", "maybe" = "maybe", "query" = "query", "PY.x" = "PY")) %>% 
+  select(AU.x, AU, DOI, TI.x, TI.y, TI, PY.x, Karlin:Abrahamse_, reviews, relevant, maybe, query, this_study)
+
+# Reconciliating TI and AU fields
+df_matched <- df_matched %>%
+  mutate(
+    AU = gsub(",.*", " et al", AU)
+  )
+
+df_matched$AU.x[is.na(df_matched$AU.x)] <- df_matched$AU[is.na(df_matched$AU.x)]
+df_matched$TI.x[is.na(df_matched$TI.x)] <- df_matched$TI[is.na(df_matched$TI.x)]
+df_matched$TI.x[is.na(df_matched$TI.x)] <- df_matched$TI.y[is.na(df_matched$TI.x)]
+
+df_matched <- df_matched %>% 
+  select(AU.x, TI.x, PY.x, DOI, Karlin:query) %>% 
+  arrange(PY.x)
+
+
+# reshape data
+
+#make paper row for plotting
+df_matchedt <- df_matched %>% 
+  mutate(
+    paper_row = row_number()
+  )
+
+table(df_matchedt$this_study, useNA = "always") #check to see numbers match with Max
